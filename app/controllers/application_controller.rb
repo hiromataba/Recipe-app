e45a4b3355dc8cf30d935836ce94f405a2332c78
Hiro Mataba <aganzemataba@gmail.com>
2021-11-18 16:23:01 +0200

    Fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   def index; end
 
   protected
