5557b51b2e2d84afa1b031a18b64b9f38b4fcbf5
Hiro Mataba <aganzemataba@gmail.com>
2021-11-18 16:11:48 +0200

    Update files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'foods#index'
+  devise_for :users
+  resources :foods, only: [:index, :create, :new, :destroy]
 end
--- db/migrate/20211111152003_add_id_to_recipe_food.rb
+++
@@ -1,5 +0,0 @@
-class AddIdToRecipeFood < ActiveRecord::Migration[6.1]
-  def change
-    add_column :recipe_foods, :id, :primary_key
-  end
-end
