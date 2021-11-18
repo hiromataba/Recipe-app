60c3660352ab1a34dd2908a2622846ff8e3e9f74
Hiro Mataba <aganzemataba@gmail.com>
2021-11-18 16:52:56 +0200

    Add spec for foods

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe Food, type: :model do
+RSpec.describe Recipe, type: :model do
   describe 'validates' do
-    subject { FactoryBot.build :food }
+    subject { FactoryBot.build :recipe }
 
     it 'should have a name' do
       subject.name = nil
       expect(subject).to_not be_valid
     end
 
-    it 'should have a measurement unit' do
-      subject.measurement_unit = nil
+    it 'should have a cooking time' do
+      subject.cooking_time = nil
       expect(subject).to_not be_valid
     end
 
-    it 'should have a price' do
-      subject.price = nil
+    it 'should have a preparation time' do
+      subject.preparation_time = nil
       expect(subject).to_not be_valid
     end
 
-    it 'should have price greater than zero' do
-      subject.price = 0
+    it 'should have description' do
+      subject.description = nil
       expect(subject).to_not be_valid
     end
   end
